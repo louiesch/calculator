fa37dcae368733664ac462aa07c5128e2898c0a8
Louie Schoenknecht <luisesch97@gmail.com>
2021-01-13 14:42:10 -0800

    add capabilities for subtraction, multiplication, and division

--- js/scripts.js
+++ js/scripts.js
@@ -26,9 +26,30 @@ function divide(number1, number2) {
 $(document).ready(function() {
   $("form#add").submit(function(event) {
     event.preventDefault();
-    const number1 = parseInt($("#add1").val());
-    const number2 = parseInt($("#add2").val());
-    const result = add(number1, number2);
-    $("#output").text(result);
+    let number1 = parseInt($("#add1").val());
+    let number2 = parseInt($("#add2").val());
+    let result = add(number1, number2);
+    $("#outputAdd").text(result);
+  });
+  $("form#subtract").submit(function(event) {
+    event.preventDefault();
+    let number1 = parseInt($("#subtract1").val());
+    let number2 = parseInt($("#subtract2").val());
+    let result = subtract(number1, number2);
+    $("#outputSubtract").text(result);
+  });
+  $("form#multiply").submit(function(event) {
+    event.preventDefault();
+    let number1 = parseInt($("#multiply1").val());
+    let number2 = parseInt($("#multiply2").val());
+    let result = multiply(number1, number2);
+    $("#outputMultiply").text(result);
+  });
+  $("form#divide").submit(function(event) {
+    event.preventDefault();
+    let number1 = parseInt($("#divide1").val());
+    let number2 = parseInt($("#divide2").val());
+    let result = divide(number1, number2);
+    $("#outputDivide").text(result);
   });
 });
